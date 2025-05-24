e938e7b031079f74ed3ec96b586c3c17e47ae668
Parth Paliwal <104494207+parthpaliwal49@users.noreply.github.com>
2025-05-24 16:45:05 +0530

    Delete exp.py

--- exp.py
+++
@@ -1,8 +0,0 @@
-import dotenv
-import os
-dotenv.load_dotenv()
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
-dotenv.load_dotenv(".env")
-key = os.getenv("GOOGLE_API_KEY")
-print(key)
-
